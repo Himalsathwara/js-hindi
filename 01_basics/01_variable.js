6f58dc731974dbe7e61e03a39fb3f69a6376f8d7
Himalsathwara <sathwarahimal700@gmail.com>
2024-08-12 16:46:22 +0000

    datatypes variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId =15441
+let accountEmail="himalgoogle.com"
+var accountPassword="1234"
+accountCity ="Jaipur"
+let accountaset =
+
+//accountId = 2//
+
+accountEmail ="hc@hc.com"
+accountPassword ="2344"
+accountCity ="Unjha"
+console.log(accountId);
+
+/*
+Prefer not to use var
+beacuse of issue in block scope aND FUNCTional
+beacuse of issue in block scope aND FUNCTional scope
+*/ 
+console.table([accountId, accountEmail, accountPassword, accountCity]) 
+
