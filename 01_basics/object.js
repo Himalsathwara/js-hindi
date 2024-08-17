cdc2da964963aba34874d343df84b6180d4dec89
Himalsathwara <sathwarahimal700@gmail.com>
2024-08-17 06:35:34 +0000

    object

---
+++ 01_basics/object.js
@@ -0,0 +1,18 @@
+const course = {
+    coustmername: "Himal",
+    price: "300",
+    Instructor: "HH"
+}
+
+const {Instructor} = course
+
+console.log(Instructor);
+
+const navbar = () =>{
+
+}
+ navbar()
+
+ {
+         name: ""
+ }
