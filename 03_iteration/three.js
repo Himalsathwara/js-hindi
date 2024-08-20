1aa7d693f7f4ffa6f0a814fbf010ab99b45a55a7
Himalsathwara <sathwarahimal700@gmail.com>
2024-08-20 17:49:21 +0000

    three

---
+++ 03_iteration/three.js
@@ -0,0 +1,39 @@
+//for of
+
+// const arr =[1,2,3,4,5]
+
+// for (const element of arr) {
+//     console.log(element);
+    
+// }
+  
+// const greetings = 'Hello world!'
+// for (const greet of greetings) {
+//     console.log(`each char is $(greet)`);
+    
+// }
+
+// Maps
+
+const map = new Map()
+map.set('IN',"india")
+map.set('USA',"United nation of america")
+map.set('Fr',"France")
+map.set('Sa',"South africa")
+
+//console.log(map);
+
+for ( const [key,value] of map ){
+    console.log(key, ':-', value);
+    }
+
+    const myObject  = {
+        'gmae1': 'NFS'
+        'game2': 'spiderman'
+    }
+     for (const [key,value] of myObject) {
+      console.log(key, ':-', value);
+      
+     }
+        
+     
